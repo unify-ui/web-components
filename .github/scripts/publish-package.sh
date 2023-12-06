455dc3d6b295cabd9445b3d5f924bd3e242bc764
luohuidong <luohuidong01@126.com>
2023-12-06 10:18:14 +0800

    ci: modify release github action

--- .github/scripts/publish-package.sh
+++ .github/scripts/publish-package.sh
@@ -11,6 +11,8 @@ is_modify=$(git status --porcelain)
 if [ ! -z "$is_modify" ]; then
     echo "packages version changed"
 
+    npm install || utils::check_fail $? "npm install failed" 
+
     git config user.name "luohuidong"
     git config user.email "luohuidong01@126.com"
     git add .
